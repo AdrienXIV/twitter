75d03ffd5e894bc86ef1ce66b39e832a5e07238b
adrien <adri_00@hotmail.fr>
2020-03-25 23:07:26 +0100

    a

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const ProtectedRoute = ({ ...props }) => {
 
 const App = () => {
   return (
-    <Router basename="/twitter">
+    <Router basename="/twitter/">
       <Navbar />
       <Switch>
         <Route exact path="/login" component={Login} />
