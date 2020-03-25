520933f18f0fba54caa839243f6c6f789a216dde
adrien <adri_00@hotmail.fr>
2020-03-25 23:23:01 +0100

    z

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./sass/style.scss";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <HashRouter basename="/">
-    <App />
+    <Switch>
+      <Route exact path="/" component={App} />
+    </Switch>
   </HashRouter>,
   document.getElementById("root")
 );
