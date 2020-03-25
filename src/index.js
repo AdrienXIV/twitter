97d20bcab43c5b187f2c3281cfdbb8193a65a54e
adrien <adri_00@hotmail.fr>
2020-03-25 21:08:09 +0100

    p

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./sass/style.scss";
-import "./js/jquery";
 
 ReactDOM.render(<App />, document.getElementById("root"));
