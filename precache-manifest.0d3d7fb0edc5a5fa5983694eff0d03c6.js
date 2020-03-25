f978acf88b063af43c8ca19f72ff712086545ee1
adrien <adri_00@hotmail.fr>
2020-03-25 22:33:27 +0100

    Updates

---
+++ precache-manifest.0d3d7fb0edc5a5fa5983694eff0d03c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e982d360b3670c9313a960819ed82a8",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "4facb7587a399a7b4c21",
+    "url": "/twitter/static/css/main.a8943c2d.chunk.css"
+  },
+  {
+    "revision": "0a2749e2b377dbf662e8",
+    "url": "/twitter/static/js/2.37b51168.chunk.js"
+  },
+  {
+    "revision": "ff9bd731c8a3c6f6da4f50e6ef6e0ad8",
+    "url": "/twitter/static/js/2.37b51168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4facb7587a399a7b4c21",
+    "url": "/twitter/static/js/main.83b2788c.chunk.js"
+  },
+  {
+    "revision": "cd520dc8eb80687d323f",
+    "url": "/twitter/static/js/runtime-main.5a7ddf07.js"
+  }
+]);
