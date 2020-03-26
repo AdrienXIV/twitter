ab87302e0b7f6682ec1fcd83028936259c964da1
adrien <adri_00@hotmail.fr>
2020-03-26 12:48:33 +0100

    a

--- src/utils/info.js
+++ src/utils/info.js
@@ -1,10 +1,10 @@
 let options = {
-  //secure: false,
-  //transports: ["websocket"],
+  secure: true,
+  transports: ["websocket"],
   autoConnect: false
 };
 let url_api = "https://project-twitter-api.herokuapp.com";
 module.exports = {
-  __URL: "http://localhost:3000", // url api
+  __URL: url_api, // url api
   __SOCKET_OPTIONS: { options } // socket.io options
 };
